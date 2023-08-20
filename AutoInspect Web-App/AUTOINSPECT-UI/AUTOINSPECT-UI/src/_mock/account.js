c1a8528d6a4ae86b3820d45a4246566cca132089
Muhammad Saqib <i190494@nu.edu.pk>
2023-08-20 23:05:26 +0500

    Delete AutoInspect Web-App/AUTOINSPECT-UI/AUTOINSPECT-UI directory

--- AutoInspect Web-App/AUTOINSPECT-UI/AUTOINSPECT-UI/src/_mock/account.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------
-
-const account = {
-  displayName: 'Vara Ali',
-  email: 'varaali85@gmail.com',
-  photoURL: '/assets/images/avatars/avatar_default.jpg',
-};
-
-export default account;
